79e50cae7311ac6b11fa6793a713845349d8e7c3
leorayjes <leorayjes@gmail.com>
2025-09-11 19:05:14 -0400

    Added backend configuration

--- providers.tf
+++ providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "my-terraform-state-08e6a14d"
+    key            = "test-course/s3/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+    
+  }
 }
 
 # Configure the AWS Provider

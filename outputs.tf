ad0ccc8d83e1728aba4e25041f36ac20452dfbc6
leorayjes <leorayjes@gmail.com>
2025-09-24 17:36:56 -0400

    Uncommented outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-# output "instance_id" {
-#   description = "The ID of the web server instance"
-#   value       = module.web_server_dev.instance_id
-# }
+output "instance_id" {
+  description = "The ID of the web server instance"
+  value       = module.web_server_dev.instance_id
+}
 
-# output "public_ip" {
-#   description = "The public IP address of the web server instance"
-#   value       = module.web_server_dev.public_ip
-# }
+output "public_ip" {
+  description = "The public IP address of the web server instance"
+  value       = module.web_server_dev.public_ip
+}
